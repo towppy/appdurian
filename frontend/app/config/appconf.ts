f3bf03dc49912c40cd520c38a5da3cbbb3665cd5
Aia Garcia <aiagarciaa2205@gmail.com>
2026-02-07 18:33:22 +0800

    conf

--- frontend/app/config/appconf.ts
+++ frontend/app/config/appconf.ts
@@ -1,15 +1,14 @@
 
 
 
-
-//export const API_URL = "https://durianapp.onrender.com";
+export const API_URL = "https://durianapp.onrender.com";
 
 // Local testing - make sure backend is running on port 5000
 //export const API_URL = "http://127.0.0.1:8000";
 
 
 
-export const API_URL = "https://untense-elaina-coltish.ngrok-free.dev";
+//export const API_URL = "https://untense-elaina-coltish.ngrok-free.dev";
 
 
 
